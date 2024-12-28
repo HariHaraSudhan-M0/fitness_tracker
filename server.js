361b44bf187a9e7cd4f1141806e47d0f5a28aac7
HARIHARASUDHAN M <m.hariharasudhan2002@gmail.com>
2024-12-28 13:03:54 +0530

    initialized route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 //express app
 const app = express()
+//route
+app.get('/',(req,res)=>{
+
+})
 //listen for requests
 app.listen(4000,()=>{
     console.log("listening on port 4000")
